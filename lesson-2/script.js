3a7ab2bb7a3588eff5baeac00172137e02ff9929
Vlad_Nozhkin <mega.nozhkin@mail.ru>
2020-11-22 21:11:15 +0300

    ready task 4

--- lesson-2/script.js
+++ lesson-2/script.js
@@ -19,19 +19,48 @@ function getRandomNumber(num) {
     return Math.floor((Math.random() - 0.5) * num);
 }
 
-    
 
+function task_4() {
+    const num = +prompt('Введите число от 0 до 15');
+    outputNum(num);
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
+function outputNum(num) {
+    switch (num) {
+        case 0:
+            console.log(num++);
+        case 1:
+            console.log(num++);
+        case 2:
+            console.log(num++);
+        case 3:
+            console.log(num++);
+        case 4:
+            console.log(num++);
+        case 5:
+            console.log(num++);
+        case 6:
+            console.log(num++);
+        case 7:
+            console.log(num++);
+        case 8:
+            console.log(num++);
+        case 9:
+            console.log(num++);
+        case 10:
+            console.log(num++);
+        case 11:
+            console.log(num++);
+        case 12:
+            console.log(num++);
+        case 13:
+            console.log(num++);
+        case 14:
+            console.log(num++);
+        case 15:
+            console.log(num++);
+        default:
+            break;    
+    }
+}
 
