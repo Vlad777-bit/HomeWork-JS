166f46d2aced1ed68149c38165bdc0437456b59c
Vlad_Nozhkin <mega.nozhkin@mail.ru>
2020-11-30 18:11:20 +0300

    task 2 is ready

--- script.js
+++ script.js
@@ -1,73 +1,36 @@
 'use strict';
 
-// Задание 1
+class Price {
+    constructor(item, price) {
+        this.item = item;
+        this.price = price;
+    }
+}
+
+const item1 = new Price('Футболка', 200);
+const item2 = new Price('Штаны', 100);
+const item3 = new Price('Куртка', 1250);
+
+const itemsSum = item1.price + item2.price + item3.price;
 
 function task_1() {
-    let i = 0;
-
-    while (i <= 100) {
-        if (!(i % 2 == 0)) {
-            console.log(i);
-        }
-        i++;
-    }
-}
-
-// function getSimpleNumber() {                 // Альтернативный вариант через for
-//     for (let i = 0; i <= 100; i++) {
-//         if (i % 2) console.log(i);
-//     }
-// }
-
-// Задание 2-3
-
-function task_2_3() {
-    const basketItems = ['Футболка', 'Штаны', 'Куртка'];
-    const ItemsCost = [
-        ['Футболка', 200],
-        ['Штаны', 100],
-        ['Куртка', 1250]
-    ];
-
-    const getItemPrice = (item, array) => {
-        const items = [];
-        const costs = [];
-
-        for (let i = 0; i < array.length; i++) {
-            items.push(array[i][0]);
-            costs.push(array[i][1]);
-        }
-
-        return costs[items.indexOf(item)];
-    };
-
-    const countBasketPrice = (basketArr, costArr) => {
-        let totalCost = 0;
-        for (let i = 0; i < basketArr.length; i++) {
-
-            totalCost += getItemPrice(basketArr[i], costArr);
-        }
-        return totalCost;
-    };
-
-    console.log('Массив корзины: ' + basketItems);
-    console.log('Массив стоимости каждого товара: ' + ItemsCost);
-    console.log('Вывод полной стоимости корзины: ' + countBasketPrice(basketItems, ItemsCost));
+    console.log(item1);
+    console.log(item2);
+    console.log(item3);
+    console.log(`Вывод полной стоимости корзины составляет: ${itemsSum} рублей`);
 }
 
 
 
-// Задание 4
-
-function task_4() {
-    for (let i = 0; i < 10; console.log(i++)) {}
-}
-
-// Задание 5
 
 
-function task_5() {
-    for (let x = 'x'; x.length <= 20; x += 'x') {
-        console.log(x);
-    }
-}
+
+
+
+
+
+
+
+
+
+
