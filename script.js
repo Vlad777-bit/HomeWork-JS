b2ddee8c3f40f162ccb58f058bf17af546c5cae1
Vlad_Nozhkin <mega.nozhkin@mail.ru>
2020-11-30 18:46:07 +0300

    task 1 is ready

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
 'use strict';
 
+// Задание 1
+
+function getDigitsNumber(num) {
+    if (!Number.isInteger(num) || num < 0 || num > 999) {
+        console.log('Вы ввели не корректное число');
+        return {};
+    }
+    
+    return {
+        number: num,
+        hundereds: Math.floor(num / 100),
+        tens: Math.floor(num / 10) % 10,
+        units: num % 10,
+    }
+}
+
+function task_1() {
+    const userNumber = +prompt('Введите целое, положительное число от 0 до 999');
+    console.log(getDigitsNumber(userNumber));
+}
+
+
+// Задание 2
+
 class Price {
     constructor(item, price) {
         this.item = item;
@@ -13,7 +37,7 @@ const item3 = new Price('Куртка', 1250);
 
 const itemsSum = item1.price + item2.price + item3.price;
 
-function task_1() {
+function task_2() {
     console.log(item1);
     console.log(item2);
     console.log(item3);
